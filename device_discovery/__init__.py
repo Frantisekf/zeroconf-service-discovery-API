6ecc42a93f5f8e2fed49722f8b7b0285b0fe8c83
Frantisek Farkas <frantisek@thefemalecompany.com>
2020-10-28 22:59:52 +0100

    added getDevices and getSingleDevices endpoints

--- device_discovery/__init__.py
+++ device_discovery/__init__.py
@@ -1,15 +1,54 @@
-import markdown
 import os
-from flask import Flask
+from flask import Flask, g
+from flask_restful import Resource, Api
+import shelve
 
-# create instance of flask
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = shelve.open("devices.db")
+    return db
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+# Create a list of MAC addresses with supported devices
+
+
+# Create an instance of Flask
 app = Flask(__name__)
 
+# Create the API
+api = Api(app)
 
-@app.route("/")
-def index():
-    with open(os.path.dirname(app.root_path) + '/README.md') as markdown_file:
 
-        content = markdown_file.read()
+class Devices(Resource):
+    def get(self):
+        shelf = get_db()
+        keys = list(shelf.keys())
 
-        return markdown.markdown(content)
+        devices = []
+
+        for key in devices:
+            devices.append(shelf[key])
+
+        return {'message': 'Success', 'data': devices}, 200
+
+
+class Device(Resource):
+    def get(self, identifier):
+        shelf = get_db()
+
+        if not (identifier in shelf):
+            return {'message': 'Device not found', 'data': {}}, 404
+
+        return {'message': 'Device found', 'data': shelf[identifier]}, 200
+
+
+api.add_resource(Devices, '/devices')
+api.add_resource(Device, '/device/<string:identifier>')

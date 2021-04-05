a4303828ba5ba8f2546b154e7dd7ea2548c0f482
frantisekfarkas <frantisekfarkas93@gmail.com>
2021-04-05 22:18:00 +0200

    Added threaded flag for request handling

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app.run(host=os.getenv('HOST'), port=os.getenv('PORT'), debug=os.getenv('DEBUG'))
+app.run(host=os.getenv('HOST'), port=os.getenv('PORT'), debug=os.getenv('DEBUG'), threaded=True)

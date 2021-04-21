7eb69e98b36eaac804ec9bc59b99a6b4d2022655
frantisekfarkas <frantisekfarkas93@gmail.com>
2021-04-22 00:20:42 +0200

    Fixed formatting

--- run.py
+++ run.py
@@ -4,5 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-if (__name__ == "__main__"):
-    app.run(host=os.getenv('HOST'), port=os.getenv('PORT'), debug=os.getenv('DEBUG'), threaded=True)
+if __name__ == "__main__":
+    app.run(
+        host=os.getenv("HOST"),
+        port=os.getenv("PORT"),
+        debug=os.getenv("DEBUG"),
+        threaded=True,
+    )

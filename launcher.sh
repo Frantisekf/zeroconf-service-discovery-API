2f20158f42dfe56938bbbc78bc4a5d9feb0ede04
frantisekfarkas <frantisekfarkas93@gmail.com>
2021-06-23 22:41:15 +0200

    Fix install script

--- launcher.sh
+++ launcher.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 #launcher.sh
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-echo $parent_path
-
-cd /
-cd $parent_path
+cd "$(dirname "$0")"
 sudo pip3 install -r requirements.txt
 sudo python3 run.py
 cd /
